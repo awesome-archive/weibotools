2ae4883e183198372dbf3a539d3d171451c1cd3b
Timmy <zhu327@gmail.com>
2013-07-21 21:23:17 +0800

    remove old version

--- weibo2rss/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.db import models
-
-class AccessToken(models.Model):
-    '''定义数据类型uid==用户微博ID，access_token==授权码，expires_in==授权过期时间'''
-    uid = models.BigIntegerField()
-    access_token = models.CharField(max_length=1000)
-    expires_in = models.BigIntegerField()
